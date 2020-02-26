07512ec6b1477b3aad49dad52612dd99b2fde38b
T.J. Yang <tjyang2001@gmail.com>
2020-02-25 19:47:50 -0500

    typos correct

--- bw-simple-test.bash
+++ bw-simple-test.bash
@@ -1,1 +1,3 @@
-time (for i in {1..20}; do IDS=('5bfd3729-7074-46f8-bbe8-aa2a00d8c0f0' 'e050ece7-2361-4415-860b-aa2a00d9d2bd' '684119e7-3039-45f3-95e3-aa2a00db18f9') FIELDS=('password' 'username'); eval "time bw-simple get ${FIELDS[$((RANDOM % ${#FIELDS[@]}+1))]} ${IDS[$((RANDOM % ${#IDS[@]}+1))]} > /dev/null"; done)
+#!/usr/bin/bash
+
+time (for i in {1..20}; do IDS=('c5bc1b67-ce78-46eb-9c41-aab100df1e03' '45ae3e3e-f038-4b53-8b58-aab201159c42' '6dc941a4-87fa-4b28-9085-aab2014b7f4a') FIELDS=('password' 'username'); eval "time bw-simple get ${FIELDS[$((RANDOM % ${#FIELDS[@]}+1))]} ${IDS[$((RANDOM % ${#IDS[@]}+1))]} > /dev/null"; done)
